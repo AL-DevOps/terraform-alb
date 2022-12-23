26ccd89e5a3dd6da82a67b3dc506d05f9aa34108
AL-DevOps <96539143+AL-DevOps@users.noreply.github.com>
2022-12-23 14:57:34 +0200

    Update security-groups.tf
    Add ports: 8080, 31900
    delete: node_group_two

--- security-groups.tf
+++ security-groups.tf
@@ -11,19 +11,22 @@ resource "aws_security_group" "node_group_one" {
       "10.0.0.0/8",
     ]
   }
+   ingress {
+   description      = "apps"
+   from_port        = 8080
+   to_port          = 8080
+   protocol         = "tcp"
+   cidr_blocks      = ["0.0.0.0/8"]
+   
+  }  
+   ingress {
+   description      = "apps1"
+   from_port        = 31900
+   to_port          = 31900
+   protocol         = "tcp"
+   cidr_blocks      = ["0.0.0.0/8"]
+   
+  }    
 }
 
-resource "aws_security_group" "node_group_two" {
-  name_prefix = "node_group_two"
-  vpc_id      = module.vpc.vpc_id
 
-  ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-
-    cidr_blocks = [
-      "10.0.0.0/8",
-    ]
-  }
-}
